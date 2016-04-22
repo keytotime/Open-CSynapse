c9f7d316598f08d5ee102caf795136d818c86731
daniel <daniel@xtihocsy01.local>
2016-04-22 16:25:00 -0600

    added a lot to infrastructure.py...should be ready pretty soon for both sides

--- infrastructure.py
+++ infrastructure.py
@@ -3,26 +3,79 @@
 from bottle import *
 import MySQLdb
 from tasks import runAlgorithm
+import json
 
 def getDB():
   db = MySQLdb.connect("localhost","csynapse","MyMZhdiEvY33WbqqAsFnLkcoQqRbacxo", "csynapse")
   return db
 
-@get('/test')
-def getTest():
+def getDescription(identifier):
+  db = getDB()
+  cursor = db.cursor()
+  select_sql = "SELECT description FROM RequestDescription WHERE identifier='%s'" % (identifier)
+  cursor.execute(select_sql)
+  result = cursor.fetchone()
+  if result != None:
+    return result[0]
+  else:
+    return ""
+
+@get('/algorithms')
+def getAlgorithms():
+  db = getDB()
+  cursor = db.cursor()
+  select_sql = "SELECT identifier, description FROM Algorithms"
+  cursor.execute(select_sql)
+  ret = ""
+  ret_list = []
+  results = cursor.fetchall()
+  for result in results:
+    res = {}
+    res["identifier"] = result[0]
+    res["description"] = result[1]
+    ret_list.append(res)
+  return json.dumps(ret_list)
+
+@get('/active_algorithms')
+def getActiveAlgorithms():
+  db = getDB()
+  cursor = db.cursor()
+  select_sql = "SELECT identifier, description FROM Algorithms WHERE active = 1"
+  cursor.execute(select_sql)
+  ret = ""
+  ret_list = []
+  results = cursor.fetchall()
+  for result in results:
+    res = {}
+    res["identifier"] = result[0]
+    res["description"] = result[1]
+    ret_list.append(res)
+  return json.dumps(ret_list)
+
+
+@get('/new')
+def getNew():
+  
+  algorithms = json.loads(getActiveAlgorithms())
+  algorithms_text = ""
+  for algorithm in algorithms:
+    algorithms_text += "<input type=\"checkbox\" name=\"algorithm\" value=\"%s\" /> %s<br />\n" % (algorithm["identifier"], algorithm["description"])
+  
   ret = """
-  <form action="test" method="POST" enctype="multipart/form-data">
+  <form action="new" method="POST" enctype="multipart/form-data">
   description <input type="text" name="description" /><br />
-  Algorithms: <input type="checkbox" name="algorithm" value="TEST" />TEST<br />
+  Algorithms: <br />"""+algorithms_text+"""
   Test File: <input type="file" name="upload" /><br />
+  <input type="hidden" name="redirect" value="/app/check" />
   <input type="submit" value="Submit">
+  
   </form>
   
   """
   return ret
 
-@post('/test')
-def postTest():
+@post('/new')
+def postNew():
   db = getDB()
   cursor = db.cursor()
   cursor.execute("SELECT MD5(UUID())")
@@ -33,13 +86,19 @@ def postTest():
     insertSQL = "INSERT INTO Requests (identifier, algorithm) VALUES (\"%s\", \"%s\")" % (newID, algorithm)
     print insertSQL
     cursor.execute(insertSQL)
+  description = request.params.get('description')
+  insertSQL = "INSERT INTO RequestDescription (identifier, description) VALUES ('%s', '%s')" % (newID, description)
+  cursor.execute(insertSQL)
   db.commit()
   upload = request.files.get('upload')
   savePath = "/var/csynapse/uploads/%s.csv" % (newID)
   upload.save(savePath)
   db.close()
   runAlgorithm.delay(newID, algorithm)
-  return "New ID: <a href=\"/app/check?id=%s\">%s</a>" % (newID, newID)
+  if request.params.get("redirect") != None and request.params.get("redirect") != "":
+    redirect(request.params.get("redirect")+"?id=%s" %(newID))
+  else:
+    return "New ID: <a href=\"/app/check?id=%s\">%s</a>" % (newID, newID)
 
 @route('/check')
 def check():
@@ -50,13 +109,26 @@ def check():
     select_sql = "SELECT identifier, algorithm, complete, return_object FROM Requests WHERE identifier=\"%s\"" % (check_id)
     cursor.execute(select_sql)
     results = cursor.fetchall()
-    for result in results:
-      ret += "ID: %s, Algorithm %s, Return Object: %s, Status: " % (result[0], result[1], result[3])
-      if result[2]==1:
-        ret += "Complete"
-      else:
-        ret += "Not Complete"
-      ret += "<br />\n"
+    human_readable = request.params.get('human_readable')
+    if human_readable == "1":
+      for result in results:
+        ret += "ID: %s, Algorithm %s, Return Object: %s, Status: " % (result[0], result[1], result[3])
+        if result[2]==1:
+          ret += "Complete"
+        else:
+          ret += "Not Complete"
+        ret += "<br />\n"
+    else:
+      retlist = []
+      for result in results:
+        result_obj = {}
+        result_obj["id"] = result[0]
+        result_obj["algorithm"] = result[1]
+        result_obj["status"] = result[2]
+        result_obj["return_object"] = result[3]
+        result_obj["description"] = getDescription(result_obj["id"])
+        retlist.append(result_obj)
+      ret = json.dumps(retlist)
   return ret
 
 @get('/all')
@@ -67,15 +139,24 @@ def getAll():
   cursor.execute(select_sql)
   ret = ""
   results = cursor.fetchall()
-  for result in results:
-    ret += "<a href=\"/app/check?id=%s\">%s</a><br />" % (result[0], result[0])
+  if request.params.get('human_readable') == "1":
+    for result in results:
+      ret += "<a href=\"/app/check?id=%s\">%s</a><br />" % (result[0], result[0])
+  else:
+    res = []
+    for result in results:
+      print result[0]
+      res.append(result[0])
+    ret_obj = {}
+    ret_obj["ids"] = res
+    ret = json.dumps(ret_obj)
   return ret
 
 @route("/")
 def index():
   links = """
-  <a href="/app/all">All Available Requests</a><br />
-  <a href="/app/test">Submit new Request</a><br />
+  <a href="/app/all?human_readable=1">All Available Requests</a><br />
+  <a href="/app/new">Submit new Request</a><br />
   """
   return links
 
