bd9e82c55d03c358a548f704dbd16b4dd2a7cb48
SamCallister <scottishsam@gmail.com>
2016-05-18 19:10:04 -0600

    changed timing to cpu time

--- MachineLearning/TimeFunction.py
+++ MachineLearning/TimeFunction.py
@@ -3,14 +3,14 @@
 
 from collections import namedtuple
 
-from time import time
+from time import clock
 # Takes a function which must come with all necessary arguments such that 
 # it can be called f(). Times the function and returns the time taken to call
 # the function.
 def timeFunction(f):
-	initial = time()
+	initial = clock()
 	returnValue = f()
-	final = time()
+	final = clock()
 	timeTaken = final - initial
 	# Tuple mapping for returning function result and timetaken
 	TimedResult = namedtuple('TimedResult','result,timeTaken')
